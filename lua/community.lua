697a78ce50383faff36ecad3d67cdabd7037a3fc
AirEnjoyer <nicholasmilligan2019@outlook.com>
2025-10-16 12:50:57 -0700

    Initial Commit

--- lua/community.lua
+++ lua/community.lua
@@ -5,6 +5,7 @@
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
+  { import = "astrocommunity.file-explorer.oil-nvim" },
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.cpp" },
   { import = "astrocommunity.pack.cmake" },
